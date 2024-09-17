e62a6d27868be7566c3c9de058e39fbd811a0c3f
Apiiyu <khoirulloh.rafi2@gmail.com>
2024-09-17 16:50:49 +0700

    style: added chalk library for better console output

--- check-and-start.js
+++ check-and-start.js
@@ -1,14 +1,17 @@
 import { execSync } from 'child_process';
 
+// Chalk
+import chalk from 'chalk';
+
 try {
   // Run eslint
-  console.log('Running eslint...');
+  console.log(chalk.blue('Running eslint...'));
   execSync('npx eslint ./src', { stdio: 'inherit' });
 
   // If no errors, run the start:dev script
-  console.log('Eslint passed. Starting the app...');
+  console.log(chalk.green('Eslint passed. Starting the app...'));
   execSync('vite', { stdio: 'inherit' });
 } catch {
-  console.error('Eslint failed. Please fix the issues before starting the app.');
+  console.error(chalk.red('Eslint failed. Please fix the issues before starting the app.'));
   process.exit(1);
 }
